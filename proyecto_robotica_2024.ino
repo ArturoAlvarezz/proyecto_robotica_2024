3d4ceb59c22706075bdff409db4ba077641ab67c
ArturoAlvarezz <arturoalvareezz@gmail.com>
2024-09-25 17:26:57 -0300

    Sensor mide a travez de pwm

--- proyecto_robotica_2024.ino
+++ proyecto_robotica_2024.ino
@@ -1,18 +1,28 @@
-// Definir el pin del LED, en este caso el pin digital 13
-int ledPin = 13;
+int pwPin = 7; // Pin donde está conectado el PWM del sensor
+long pulseWidth; // Ancho del pulso en microsegundos
+float distanceInInches;
+float distanceInCm;
 
 void setup() {
-  // Configurar el pin del LED como salida
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);  // Inicializa la comunicación serial
+  pinMode(pwPin, INPUT);  // Configura el pin del sensor como entrada
 }
 
 void loop() {
-  // Encender el LED
-  digitalWrite(ledPin, HIGH);
-  // Esperar 1000 milisegundos (1 segundo)
-  delay(1000);
-  // Apagar el LED
-  digitalWrite(ledPin, LOW);
-  // Esperar 1000 milisegundos (1 segundo)
-  delay(1000);
+  // Lee el ancho del pulso en microsegundos
+  pulseWidth = pulseIn(pwPin, HIGH);
+
+  // Convierte el ancho del pulso en distancia (usando 147us/inch)
+  distanceInInches = pulseWidth / 147.0;
+
+  // Convierte la distancia a centímetros
+  distanceInCm = distanceInInches * 2.54;
+
+  // Imprime la distancia
+  Serial.print("Distancia: ");
+  Serial.print(distanceInCm);
+  Serial.println(" cm");
+  
+
+  delay(100);  // Espera 1 segundo antes de tomar otra lectura
 }
